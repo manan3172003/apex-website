2d7a3926109ade765f362cb98308af436cc1f025
manan3172003 <mjain2@ualberta.ca>
2023-05-23 00:43:04 -0600

    some more changes to navbar

--- src/components/NavBar2.js
+++ src/components/NavBar2.js
@@ -5,7 +5,7 @@ import { Menu } from "@mui/material";
 import { MenuItem } from "@mui/material";
 import { Avatar } from "@mui/material";
 import { Tooltip } from "@mui/material";
-import { Adb as AdbIcon } from "@mui/icons-material";
+import './NavBar2.css'
 import { Menu as MenuIcon } from "@mui/icons-material";
 import React from "react";
 import logo from '../assets/logo1.png';
@@ -35,135 +35,111 @@ export const NavBar2 = () => {
 
   return (
     <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <AdbIcon src={logo} sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <SvgIcon>
-            <path d='../assets/logo1.png' />
-          </SvgIcon>
+    <Container maxWidth="xl">
+      <Toolbar disableGutters>
+        <Box sx={{ flexGrow: 1, display: { xs: 'flex', sm: 'flex', md: 'none', lg: 'none' } }}>
+          <IconButton
+            size="medium"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            color="inherit"
+            marginRight="0px"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{
+              display: { xs: 'block', md: 'none' },
+            }}
+          >
+            {pages.map((page) => (
+              <MenuItem key={page} onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">{page}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+  
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <img src={logo} alt="logo" style={{ width: '60px', height: '60px', marginRight: '8px' }} />
           <Typography
             variant="h6"
             noWrap
-            component="a"
+            component="p"
             href="/"
             sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
               fontWeight: 700,
               letterSpacing: '.2rem',
               color: 'secondary.main',
               textDecoration: 'none',
               '&:hover': {
-                // Add the same styles as the default state
-                mr: 2,
-                display: { xs: 'none', md: 'flex' },
                 fontWeight: 700,
                 color: 'secondary.main',
                 letterSpacing: '.2rem',
                 textDecoration: 'none',
-              }
+              },
+              display: { xs: 'none', md: 'flex' },
             }}
           >
             ApexTravels
           </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
           <Typography
             variant="h5"
             noWrap
-            component="a"
+            component="p"
             href=""
             sx={{
               mr: 2,
               display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
               fontWeight: 700,
               letterSpacing: '.2rem',
-              color: 'inherit',
+              color: 'secondary.main',
               textDecoration: 'none',
+              alignSelf: 'center',
+              '&:hover': {
+                mr: 2,
+                display: { xs: 'flex', md: 'none' },
+                fontWeight: 700,
+                color: 'secondary.main',
+                letterSpacing: '.2rem',
+                textDecoration: 'none',
+              },
             }}
           >
             ApexTravels
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
+        </Box>
+  
+        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          {pages.map((page) => (
+            <Button
+              key={page}
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, color: 'white', display: 'block' }}
             >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+              {page}
+            </Button>
+          ))}
+        </Box>
+      </Toolbar>
+    </Container>
+  </AppBar>
+  
+
   );
 };
